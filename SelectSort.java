16120fb4cafb25465788b0b1611c6ac5cc5553aa
Andrew <aleut@umich.edu>
2020-12-03 22:27:39 -0500

    Mixed and Duplicate tests

---
+++ SelectSort.java
@@ -0,0 +1,98 @@
+public class SelectionSort {
+	 private int temp;
+
+	/** Creates a new instance of SelectionSort */
+   public SelectionSort() {
+   }
+
+   /* A simple SelectionSort algorithm
+    * pre-condition:
+    * post-condition:
+    * inputs:
+    * outputs:
+    * special conditions:
+    */
+   public int[] basicSelectionSort(int[] x) {
+       for (int i = 0; i < x.length; ++i) {
+           for (int j= i+1; j < x.length; ++j) {
+               if (x[i] > x[j]) {
+                   temp = x[i];
+                   x[i] = x[j];
+                   x[j] = temp;
+               }
+           } // end of inner for loop
+       } // end of outer for loop
+       return x;
+   }
+
+}
+
+import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSelectionSort {
+	@Test
+
+	public void test() {
+		testPositive();
+		testNegative();
+		testMixed();
+		testDuplicates();
+	}
+
+  public TestSelectionSort() {
+  }
+
+  public void testMixed(){
+        int[] arr = new int[5];
+        arr[0] = -8;
+        arr[1] = 9;
+        arr[2] = -7;
+        arr[3] = 10;
+        arr[4] = -2;
+
+        SelectionSort sort = new SelectionSort();
+        sort.basicSelectionSort(arr);
+
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -8;
+        Sortedarr[1] = -7;
+        Sortedarr[2] = -2;
+        Sortedarr[3] = 9;
+        Sortedarr[4] = 10;
+
+        assertSame("Failed Test", Sortedarr[4], arr[4]);
+        assertSame("Failed Test", Sortedarr[3], arr[3]);
+        assertSame("Failed Test", Sortedarr[2], arr[2]);
+        assertSame("Failed Test", Sortedarr[1], arr[1]);
+        assertSame("Failed Test", Sortedarr[0], arr[0]);
+
+    }
+
+    public void testDuplicates(){
+    	int[] arr = new int[5];
+        arr[0] = 8;
+        arr[1] = 9;
+        arr[2] = 8;
+        arr[3] = 10;
+        arr[4] = 9;
+
+        SelectionSort sort = new SelectionSort();
+        sort.basicSelectionSort(arr);
+
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = 8;
+        Sortedarr[1] = 8;
+        Sortedarr[2] = 9;
+        Sortedarr[3] = 9;
+        Sortedarr[4] = 10;
+
+        assertSame("Failed Test", Sortedarr[4], arr[4]);
+        assertSame("Failed Test", Sortedarr[3], arr[3]);
+        assertSame("Failed Test", Sortedarr[2], arr[2]);
+        assertSame("Failed Test", Sortedarr[1], arr[1]);
+        assertSame("Failed Test", Sortedarr[0], arr[0]);
+
+    }
+}
